6a38f9727aef38e613eb560e6d1163880cbb82b1
canislupus.pyros@gmail.com <canislupus.pyros@gmail.com>
2013-11-05 13:30:23 +0000

    ..And so it begins..

---
+++ comic/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Comic::Application.config.secret_key_base = '608a847b6eef0b5ed51a18ab931e29da4b14f110143c7920f6b3649c8dea88929862477cf93a1549f2983ff94241d3c2625a3a49a603a78113cda82be80312bd'
---
+++ prettanike/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prettanike::Application.config.secret_token = 'd9bf4947aadfd22786a229abd316e01ff3f20d06845c0d733bd99a123f4d138963e000e0a548b230a950bd7aab2f58f148acfe1a9a62cc76e83431b37ca96784'
